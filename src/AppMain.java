4ed7360ab3039836f6fb56c00a377e037c89528f
LEONARDO CONTRERAS MARTINEZ <L21051403@saltillo.tecnm.mx>
2024-02-07 23:29:46 -0600

    Tarea 1 de automatas xd

--- src/AppMain.java
+++ src/AppMain.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class AppMain {
@@ -12,11 +14,8 @@ public class AppMain {
     static JTextField edad = new JTextField("Edad");
     static ArrayList<String> list = new ArrayList<>();
     static File salida = new File("Salida.txt");
-
-
     public static void main(String[] args) throws IOException {
         CrearArchivo();
-        FileWriter writer = new FileWriter(salida, true);
         int opc = 0;
         String menu = """
                     1. Capturar
@@ -35,13 +34,9 @@ public class AppMain {
                         "Edad", edad
                 };
                 switch(opc){
-                    case 1 ->{
-                        capturarDatos(fields);
-                        writer.append("\nola");
-                        writer.close();
-                    }
+                    case 1 -> capturarDatos(fields);
                     case 2->{
-
+                        mostrarDatosLeidos();
                     }
                 }
             }
@@ -50,32 +45,68 @@ public class AppMain {
 
     static void CrearArchivo() throws IOException {
         FileWriter writer;
-        writer = new FileWriter(salida);
         if(salida.exists()){
             Scanner sc = new Scanner(salida);
             int lineas = 0;
             while(sc.hasNextLine()){
                 lineas++;
                 sc.nextLine();
+                System.out.println("a");
             }
-            if(lineas < 1)
+            System.out.println(lineas);
+            if(lineas == 0) {
+                writer = new FileWriter(salida);
                 writer.write("|     NOMBRE      |       APELLIDO PATERNO         |     APELLIDO MATERNO         |          EDAD       |");
+                writer.close();
+            }
         }else
             salida.createNewFile();
-        writer.close();
     }
 
-    public static void capturarDatos(Object[] fields){
-        int cantidad = Integer.parseInt(JOptionPane.showInputDialog(null,"Cuantos datos desea agregar?"));
+    public static void capturarDatos(Object[] fields) throws IOException {
         JOptionPane.showMessageDialog(null,fields,"Selecciona",JOptionPane.OK_CANCEL_OPTION);
         if(isNullOrBlank(nombre.getText()) || isNullOrBlank(apPaterno.getText()) || isNullOrBlank(apMaterno.getText()) ||
                 !checkNumeric(edad.getText())) {
             JOptionPane.showMessageDialog(null, "Olvido llenar un dato o introduzco un valor no numerico en edad! ");
-            list.add(nombre.getText());
-            list.add(apPaterno.getText());
+            return;
         }
-        else
-            JOptionPane.showMessageDialog(null,"Datos almacenados correctamente!");
+        JOptionPane.showMessageDialog(null,"Datos almacenados correctamente!");
+        Persona persona = new Persona(nombre.getText(), apPaterno.getText(), apMaterno.getText(), Integer.parseInt(edad.getText()));
+        escribirArchivo(persona);
+    }
+
+    public static void escribirArchivo(Persona persona) throws IOException {
+        FileWriter writer = new FileWriter(salida,true);
+        writer.append(String.format("\n| %s | %s | %s | %d |",persona.getNombre(),persona.getApPaterno(),persona.getApMaterno(),persona.getEdad()));
+        writer.close();
+    }
+
+    public static void mostrarDatosLeidos() {
+        Scanner sc = null;
+        Object[] cols = {"Nombre", "Apellido Paterno", "Apellido Materno", "Edad"};
+        boolean exists = false;
+        try{
+            sc = new Scanner(salida);
+            exists = true;
+        }catch (FileNotFoundException e){
+            JOptionPane.showMessageDialog(null,"El archivo aun no existe! seleccione la opcion 1 primero");
+        }
+        if(!exists) return;
+        ArrayList<String[]> list = new ArrayList<>();
+        int amount = 0;
+        while(sc.hasNextLine()){
+            String line = sc.nextLine().substring(2);
+            String[] arr = line.split("\\|");
+            if(arr.length == 0) continue;
+            list.add(arr);
+            amount++;
+        }
+        Object[][] rows = new Object[amount][4];
+        for (int i = 1; i < amount; i++)
+            for (int j = 0; j < 4; j++)
+                rows[i][j] = list.get(i)[j];
+        JTable table = new JTable(rows,cols);
+        JOptionPane.showMessageDialog(null,new JScrollPane(table));
     }
 
     public static boolean isNullOrBlank(String s){
---
+++ src/Persona.java
@@ -0,0 +1,51 @@
+public class Persona {
+    private String nombre;
+    private String apPaterno;
+    private String apMaterno;
+    private int edad;
+
+    public Persona(){
+    }
+
+    public Persona(String nombre, String apPaterno, String apMaterno, int edad){
+        this.apPaterno = apPaterno;
+        this.apMaterno = apMaterno;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApPaterno() {
+        return apPaterno;
+    }
+
+    public void setApPaterno(String apPaterno) {
+        this.apPaterno = apPaterno;
+    }
+
+    public String getApMaterno() {
+        return apMaterno;
+    }
+
+    public void setApMaterno(String apMaterno) {
+        this.apMaterno = apMaterno;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+
+
+}

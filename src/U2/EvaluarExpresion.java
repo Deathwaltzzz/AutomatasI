199a6307be671706c345f00d0fdafa7b69a6697a
LEONARDO CONTRERAS MARTINEZ <L21051403@saltillo.tecnm.mx>
2024-02-25 19:19:18 -0600

    Practica 2 (en sucio con comentarios)

--- src/U2/EvaluarExpresion.java
+++ src/U2/EvaluarExpresion.java
@@ -1,4 +1,5 @@
 package U2;
+
 import java.util.LinkedList;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -6,13 +7,21 @@ import java.util.regex.Matcher;
 public class EvaluarExpresion {
     private String expresion;
     private LinkedList<String> coinciden;
-    private LinkedList<String > noCoincide;
-    public EvaluarExpresion(String[] cadenas, int lenguajeAnalizar){
+    private LinkedList<String> noCoincide;
+
+    public EvaluarExpresion(String[] cadenas, int lenguajeAnalizar) {
         coinciden = new LinkedList<>();
         noCoincide = new LinkedList<>();
-        switch (lenguajeAnalizar){
-            case 1 -> {for (String cadena : cadenas) analizarLenguaje1(cadena);}
-            case 3 -> {for (String cadena : cadenas) exReg_con_regex(cadena);}
+        switch (lenguajeAnalizar) {
+            case 1 -> {
+                for (String cadena : cadenas) analizarLenguaje1(cadena);
+            }
+            case 2 -> {
+                for (String cadena : cadenas) exReg_pattern(cadena);
+            }
+            case 3 -> {
+                for (String cadena : cadenas) lenguaje2(cadena);
+            }
         }
         System.out.println("Coincide: " + coinciden.toString());
         System.out.println("No coinciden: " + noCoincide.toString());
@@ -20,8 +29,8 @@ public class EvaluarExpresion {
 
     /*Este metodo comprueba caracter por caracter si la cadena pertenece a la expresion regylar "(a | e | i | o | u)+ b (0-9)*"
      * Recibe como parametro el string cadena, el cual ira desglozando y comprobando si pertenece o no a la expresion regular*/
-    public void analizarLenguaje1(String cadena){
-        if(!esVocal(cadena.charAt(0)) || cadena.length() == 1){
+    public void analizarLenguaje1(String cadena) {
+        if (!esVocal(cadena.charAt(0)) || cadena.length() == 1) {
             noCoincide.add(cadena);
             return;
         }
@@ -29,59 +38,115 @@ public class EvaluarExpresion {
         int aux = 0;
         boolean togVocal = false, togNum = false, togConst = false;
         for (int i = 0; i < cadena.length(); i++) {
-            char currChar = Character.toLowerCase(cadena.charAt(i));
+            char currChar = cadena.charAt(i);
             aux = i;
-            while(esVocal(currChar) && !togVocal){ //Este metodo se activa una vez que se detecte una vocal, terminara su ejecucion cuando ya no existan mas vocales.
-                char lowerCase = Character.toLowerCase(cadena.charAt(aux + 1));
-                if(lowerCase == 'b' || aux == cadena.length() - 2){ //Una vez que se analize todas las vocales se comprueba si la siguiente letra es 'b', si es asi se continuara con la evaluiacion.
+            while (esVocal(currChar) && !togVocal) { //Este metodo se activa una vez que se detecte una vocal, terminara su ejecucion cuando ya no existan mas vocales.
+                if (cadena.charAt(i+1) == 'b' || aux == cadena.length() - 2) { //Una vez que se analize todas las vocales se comprueba si la siguiente letra es 'b', si es asi se continuara con la evaluiacion.
                     togVocal = true;
-                    currChar = lowerCase;
+                    currChar = cadena.charAt(i+1);
                     i = aux + 1;
                     break;
                 }
                 aux++;
             }
-            if(!togVocal) continue; //Valida que exista aunque sea una vocal dentro de la cadena
-            if(currChar != 'b' && !togConst) break; //Si el caracter actual no es b y no se ha toggleado el boolean de es constante se crasheara el ciclo
-            if(togConst && currChar == 'b' || togConst && esUnaLetra(currChar)) break; //Si hay mas de 1 b o una letra despues del a b
+            if (!togVocal) continue; //Valida que exista aunque sea una vocal dentro de la cadena
+            if (currChar != 'b' && !togConst)
+                break; //Si el caracter actual no es b y no se ha toggleado el boolean de es constante se crasheara el ciclo
+            if (togConst && currChar == 'b' || togConst && esUnaLetra(currChar))
+                break; //Si hay mas de 1 b o una letra despues del a b
             togConst = true; //Se togglea la consstante
             togNum = esNumerico(currChar); //Se togglea si es un numero el caracter actual
-            if(cadena.length()-1 == i && !togNum) coincide = true; //Se comrpueba si la ultima letra es una b y no se ha detectado algun numero
-            if(!(togNum && i == cadena.length() - 1)) continue; //Si aun no se detecta un numero y la i aun no llega a su limite se sigue comprobando
+            if (cadena.length() - 1 == i && !togNum)
+                coincide = true; //Se comrpueba si la ultima letra es una b y no se ha detectado algun numero
+            if (!(togNum && i == cadena.length() - 1))
+                continue; //Si aun no se detecta un numero y la i aun no llega a su limite se sigue comprobando
             coincide = true; //Si salta todos los puntos de control, significara que la cadena SI pertenece a la expresion regular
         }
-        if(!coincide) noCoincide.add(cadena);
+        if (!coincide) noCoincide.add(cadena);
         else coinciden.add(cadena);
     }
 
     public boolean esVocal(char caracter) { //Metodo para decir si la letra es vocal o no
-        char caracterMinuscula = Character.toLowerCase(caracter);
-        return (caracterMinuscula  == 'a' || caracterMinuscula == 'e' || caracterMinuscula == 'i' || caracterMinuscula == 'o' || caracterMinuscula == 'u');
+        return (caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u');
     }
 
-    public boolean esUnaLetra(char letra){ // Metodo para decir si es una letra o no
-        return (letra >= 65 && letra <=90) || (letra >= 97 && letra <= 122);
+    public boolean esUnaLetra(char letra) { // Metodo para decir si es una letra o no
+        return (letra >= 65 && letra <= 90) || (letra >= 97 && letra <= 122);
     }
 
-    public boolean esNumerico(char numero){ //Metodo para decir si es un caracter numerico o no
+    public boolean esNumerico(char numero) { //Metodo para decir si es un caracter numerico o no
         return (numero - '0') >= 0 && ((numero - '0') < 10);
     }
 
-    public void exReg_con_regex(String cadena) {
-        String expReg = "^(ab|xy)+&\\d*";
-        Pattern pattern = Pattern.compile(expReg);
-        Matcher matcher = pattern.matcher(cadena);
-        if (matcher.find())
-            coinciden.add(cadena);
-        else
+    //ADRIAN
+    //para la expresion Expresión Regular: a(a-z)*(1|3|5|7|9)+
+    public void lenguaje2(String cadena) {
+        boolean coincide = false, imparEncontrado = false;
+        if (cadena.charAt(0) != 'a' || cadena.length() == 1) { //Si el primer caracter no es 'a' entonces no tiene porque seguir con la ejecucion del programa, aqui se puede terminar de una.
             noCoincide.add(cadena);
+            return;
+        }
+        for(int i = 0 ; i < cadena.length() ; i++) {
+        /*contador++;// aumenta el contador
+        while (contador < tamaño) // verificar si el contador es menor al tamaño de la cadena
+        {*/
+            char caracter = cadena.charAt(i);// se extrae el caracter con la posicion del caracter
+            if(!(esNumerico(caracter) || esUnaLetra(caracter))) break;
+            if(esUnaLetra(caracter) && !imparEncontrado) continue;
+            if(esUnaLetra(caracter) && imparEncontrado) break;
+            /*En vez de utilizar un contador, utilizo booleanos para checar si es una letra y no se ha detectado un numero impoar que siga con el programa. Si no que termine
+            * Es basicamente lo mismo pero mas simplificado.*/
+
+            /*if (caracter >= 97 && caracter <= 122) // si ese caracter es una letra minuscula
+            {
+                // ================================
+                System.out.println(contador);    // 1  //Comentario de Leo:
+                // ================================      Siento que es algo redundante checar si existe algo entre a y ( 1 | 3 | 5 | 7 | 9)
+                contador++;                              Si algo se tendria que verificar primero si hay un caracter no perteneciente o un numero par
+
+            } else */
+            //if(esNumerico(caracter))   // es numerico? Comentario Leo: En vez de checar si es numerico y ejecutar, mejor checa si no es numerico, checar de una si no es par, mas facil y optimizado
+            //int numero = Character.getNumericValue(caracter);// trae el valor numerico del caracter, no el ascii
+            //if (caracter == 1 || caracter == 3 || caracter == 5 || caracter == 7 || caracter == 9) // ese numero es impar? Comentario Leo: Lo mismo aqui, en vez de checar si si es, checar si no es, si no es un numero impar truena el programa.
+            if (!esImpar(caracter)) break;//Comentario Leo: Es mas facil hacer una division entre 2, si es 1 eso significa que es impar
+               /* if (caracter == Character.getNumericValue(cadena.charAt(contador))) //si ese numero es igual al otro. Comentario Leo:
+                    noCoincide.add(cadena);// se agrega alas cadenas que no coinciden                                       Segun yo las cadenas dentro de un (1 | 3 | 4...) se pueden repetir.
+                    break;
+                }*/
+            /*Nota de Leo:
+            * En esta parte del codigo, es mejor ir checando condicion por condicion, si se cumple sigue ejecutandote hasta llegar a lo que queremos
+            * Si no. rompe el ciclo y agregalo a lo que no pertenece. El problema con la implementacion original era que no tenia bien establecido
+            * los puntos de control del programa*/
+            imparEncontrado = true;
+            if( i < cadena.length() - 1 && esImpar(caracter) && esNumerico(cadena.charAt(i+1))) continue;
+            if(cadena.length()-1 == i) coincide = true;
+        }
+        if(coincide) coinciden.add(cadena);
+        else noCoincide.add(cadena);
+        //} //se termina el while
     }
 
-    public String[] returnCoincidencias(){ // Metodo que regresa un arreglo con las cadenas que coinciden de la expresion regular
+    public boolean esImpar(char num){
+        return ((int) num)%2 == 1;
+    }
+
+    public void exReg_pattern(String cadena) {
+        String ER2 = "a[a-z0-9]*(1|3|5|7|9)+";
+        Pattern compilador = Pattern.compile(ER2);
+        Matcher comparador = compilador.matcher(cadena);
+        //MUESTRA SI LA EXPRESION SI PERTENECE O NO A LA ER
+        if (comparador.matches()) {
+            coinciden.add(cadena);
+        } else {
+            noCoincide.add(cadena);
+        }
+    }
+
+    public String[] returnCoincidencias() { // Metodo que regresa un arreglo con las cadenas que coinciden de la expresion regular
         return coinciden.toArray(new String[0]);
     }
 
-    public String[] returnNoCoincidencias(){ //Metodo que regresa las cadenas que NO coinciden en la expresion regular
+    public String[] returnNoCoincidencias() { //Metodo que regresa las cadenas que NO coinciden en la expresion regular
         return noCoincide.toArray(new String[0]);
     }
 
--- src/U2/Practica2.java
+++ src/U2/Practica2.java
@@ -1,6 +1,7 @@
 package U2;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -21,44 +22,34 @@ public class Practica2 {
         String menu = "\nMenú \n";
         menu += "Eliga una opción para ver las cadenas validas y no validas para cada expresión \n";
         menu += "1.  ER =(a|e|i|o|u)+b_(09)*  \n";
-        menu += "2.  ER = \n";
+        menu += "2.  ER = a[a-z0-9]*(1|3|5|7|9)+\n";
         menu += "3.  ER = (ab|xy)+&(0-9)*  \n";
         menu += "4.  Salir  \n";
         opciones = 4;
+
+        String regex = null;
 
         do {
             opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
             switch (opcion) {
                 case 1:
                     evaluador = new EvaluarExpresion(texto,1);
+                    regex = "(a | e | i | o | u)+ b (0-9)*";
                     break;
                 case 2:
-                    // porción de código
+                    evaluador = new EvaluarExpresion(texto,2);
+                    regex = "a[a-z0-9]*(1|3|5|7|9)";
                     break;
                 case 3:
                     evaluador = new EvaluarExpresion(texto,3);
+                    regex = "(ab|xy)+&(0-9)*";
                     break;
                 case 4:
                     JOptionPane.showMessageDialog(null, "Fin del programa");
                     break;
             }
-            if(evaluador != null && opcion != 4) {
-                JOptionPane.showMessageDialog(null, String.format("""
-                                Numero de coincidencias:\s
-                                %d\s
-                                Caracteres que coincidieron:\s
-                                %s""",
-                        evaluador.returnCoincidencias().length, Arrays.toString(evaluador.returnCoincidencias()))
-                );
-                JOptionPane.showMessageDialog(null, String.format("""
-                                Numero de NO coincidencias:\s
-                                %d\s
-                                Caracteres que NO coincidieron:\s
-                                %s""",
-                        evaluador.returnNoCoincidencias().length, Arrays.toString(evaluador.returnNoCoincidencias()))
-                );
-            }
-
+            if(evaluador != null && opcion != 4)
+                mostrarResultados(evaluador.returnCoincidencias(), evaluador.returnNoCoincidencias(),regex);
         } while (opcion != opciones);
     }
 
@@ -73,7 +64,6 @@ public class Practica2 {
             JOptionPane.showMessageDialog(null,"El archivo no existe! asegurese de crear un archivo con el nombre \"texto.txt!\"");
             return null;
         }
-
         ArrayList<String> x = new ArrayList<>();
         while(sc.hasNextLine()){
             String[] line = sc.nextLine().trim().split("\\s+");
@@ -81,5 +71,27 @@ public class Practica2 {
         }
         if(x.isEmpty()) return null;
         return x.toArray(new String[0]);
+    }
+
+    public static void mostrarResultados(String[] coincidencia, String[] noCoincidencia, String regex){
+        Object[] cols = {"Coinciden", "Cantidad", "No coinciden", "Cantidad"};
+        int cantidad = Math.max(coincidencia.length, noCoincidencia.length);
+        Object[][] rows = new Object[cantidad][4];
+        for (int i = 0; i < coincidencia.length; i++)
+            rows[i][0] = coincidencia[i];
+        for (int i = 0; i < noCoincidencia.length; i++)
+            rows[i][2] = noCoincidencia[i];
+        rows[0][1] = String.valueOf(coincidencia.length);
+        rows[0][3] = String.valueOf(noCoincidencia.length);
+
+        JLabel label = new JLabel("EVALUACION DEL REGEX " + regex);
+        JTable table = new JTable(rows, cols);
+        JPanel container = new JPanel();
+        container.setLayout(new BorderLayout());
+        container.add(label, BorderLayout.PAGE_START); // Place label at the top
+        container.add(table, BorderLayout.CENTER);
+        JScrollPane pane = new JScrollPane(container);
+        pane.setColumnHeaderView(table.getTableHeader());
+        JOptionPane.showMessageDialog(null, pane);
     }
 }

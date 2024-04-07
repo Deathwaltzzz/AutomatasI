c41546b2a2c0d3163d958689e1fc1c74d05c8a9d
LEONARDO CONTRERAS MARTINEZ <L21051403@saltillo.tecnm.mx>
2024-04-07 13:35:58 -0600

    Analizador lexico (posiblemente terminado)

--- src/U3/Practica4_Eq1.java
+++ src/U3/Practica4_Eq1.java
@@ -4,7 +4,9 @@ import java.io.File;
 
 public class Practica4_Eq1 {
     public static void main(String[] args) {
-        AnalizadorLexico al = new AnalizadorLexico();
-        al.leerArchivo(new File("entrada.txt"));
+        if(args.length == 0)
+            new AnalizadorLexico().leerArchivo(new File("entrada.txt"));
+        else
+            new AnalizadorLexico().leerArchivo(new File(args[0]));
     }
 }
